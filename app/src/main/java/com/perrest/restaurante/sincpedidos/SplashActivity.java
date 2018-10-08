f58c8a33a2fc5621039671951289df4bf52e419e
Felipe Galeote <felipe10gale@gmail.com>
2018-10-08 00:11:45 -0300

    Adiciona animação na tela de carregamento

--- app/src/main/java/com/perrest/restaurante/sincpedidos/SplashActivity.java
+++ app/src/main/java/com/perrest/restaurante/sincpedidos/SplashActivity.java
@@ -2,14 +2,36 @@ package com.perrest.restaurante.sincpedidos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        logo = findViewById(R.id.splash_logo);
 
+        logo.post(() -> rotacionarView(logo,1800));
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    private void rotacionarView(View view, int duracao){
+        RotateAnimation animacao = new RotateAnimation(0.0f,360.0f,view.getWidth()/2,view.getHeight());
+        animacao.setInterpolator(new AccelerateDecelerateInterpolator());
+        animacao.setDuration(duracao);
+        animacao.setRepeatCount(Animation.INFINITE);
+        view.startAnimation(animacao);
     }
 }
